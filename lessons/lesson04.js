db3f19290099e53387006ada340ea883993ebd7a
JulioAsecEd <ed_test_julio@outlook.com>
2024-09-26 08:25:51 -0300

    lesson04

---
+++ lessons/lesson04.js
@@ -0,0 +1,31 @@
+let arrayPessoas = [
+    {nome: "Carlos Batata",
+     idade: 16,
+     cidade: "Vale do Rio Salgado"   
+    },
+    
+    {nome: "Salmão Rodrigues",
+     idade: 60,
+     cidade: "Jarabaguá"   
+    },
+    
+    {nome: "Raimundo Nonato",
+     idade: 73,
+     cidade:  "Escolinha"  
+    },
+    
+    {nome: "José das Couves",
+    idade: 33,
+    cidade:  "Milharal"  
+    }
+]
+
+for (const elem of arrayPessoas) {
+    console.log("Nome: " + elem.nome);
+    console.log("Idade: "+ elem.idade);
+    console.log("Cidade: "+ elem.cidade);
+    console.log(" ");
+        
+    
+    
+}
